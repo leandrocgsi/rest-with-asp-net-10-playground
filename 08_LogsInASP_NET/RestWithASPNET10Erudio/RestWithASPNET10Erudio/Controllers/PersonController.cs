398677222015c5ca4298dc18d88a9ab40e6a01bc
leandrocgsi <leandrocgsi@gmail.com>
2025-08-17 14:32:35 -0300

    Logs in Microsoft ASP.NET 10 Applications

---
+++ 08_LogsInASP_NET/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Controllers/PersonController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASPNET10Erudio.Model;
+using RestWithASPNET10Erudio.Services;
+
+namespace RestWithASPNET10Erudio.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private IPersonServices _personService;
+
+        public PersonController(IPersonServices personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            var createdPerson = _personService.Create(person);
+            if (createdPerson == null) return NotFound();
+            return Ok(createdPerson);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            var createdPerson = _personService.Update(person);
+            if (createdPerson == null) return NotFound();
+            return Ok(createdPerson);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+    }
+}
