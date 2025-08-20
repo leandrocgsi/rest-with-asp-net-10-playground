5e9d65cdf75f9407f16119752f2c40371ce868d6
leandrocgsi <leandrocgsi@gmail.com>
2025-08-19 21:58:58 -0300

    Splitting Business Logic in Layered Architecture

---
+++ 09_Layered_Architecture/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Controllers/PersonController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASPNET10Erudio.Model;
+using RestWithASPNET10Erudio.Services;
+
+namespace RestWithASPNET10Erudio.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private IPersonServices _personService;
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(IPersonServices personService,
+            ILogger<PersonController> logger)
+        {
+            _personService = personService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Fetching all persons");
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            _logger.LogInformation("Fetching person with ID {id}", id);
+            var person = _personService.FindById(id);
+            if (person == null)
+            {
+                _logger.LogWarning("Person with ID {id} not found", id);
+                return NotFound();
+            }
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            _logger.LogInformation("Creating new Person: {firstName}", person.FirstName);
+
+            var createdPerson = _personService.Create(person);
+            if (createdPerson == null)
+            {
+                _logger.LogError("Failed to create person with name {firstName}", person.FirstName);
+                return NotFound();
+            }
+            return Ok(createdPerson);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            _logger.LogInformation("Updating person with ID {id}", person.Id);
+
+            var createdPerson = _personService.Update(person);
+            if (createdPerson == null)
+            {
+                _logger.LogError("Failed to update person with ID {id}", person.Id);
+                return NotFound();
+            }
+            _logger.LogDebug("Person updated successfully: {firstName}", createdPerson.FirstName);
+            return Ok(createdPerson);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _logger.LogInformation("Deleting person with ID {id}", id);
+            _personService.Delete(id);
+            _logger.LogDebug("Person with ID {id} deleted successfully", id);
+            return NoContent();
+        }
+    }
+}
---
+++ 09_Layered_Architecture/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Program.cs
@@ -0,0 +1,24 @@
+using RestWithASPNET10Erudio.Configurations;
+using RestWithASPNET10Erudio.Services;
+using RestWithASPNET10Erudio.Services.Impl;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddSerilogLogging();
+
+builder.Services.AddControllers();
+
+builder.Services.AddDatabaseConfiguration(builder.Configuration);
+builder.Services.AddScoped<IPersonServices, PersonServicesImpl>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
