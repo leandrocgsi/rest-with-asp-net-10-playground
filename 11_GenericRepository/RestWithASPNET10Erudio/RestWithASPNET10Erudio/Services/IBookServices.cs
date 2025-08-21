47c613ec6b4e9b3478a376770b88f3762e583214
leandrocgsi <leandrocgsi@gmail.com>
2025-08-21 09:30:04 -0300

    Working With Generic Repository

---
+++ 11_GenericRepository/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Services/IBookServices.cs
@@ -0,0 +1,18 @@
+﻿using RestWithASPNET10Erudio.Model;
+
+namespace RestWithASPNET10Erudio.Services
+{
+    public interface IBookServices
+    {
+
+        Book Create(Book book);
+
+        Book FindById(long id);
+
+        List<Book> FindAll();
+
+        Book Update(Book book);
+
+        void Delete(long id);
+    }
+}
