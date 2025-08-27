50ab15dadddfc40cc554fbae78080924b5aa5abd
leandrocgsi <leandrocgsi@gmail.com>
2025-08-27 14:16:13 -0300

    Versioning RESTful API's with ASP.NET 10 and .NET 10

--- 13_VersioningRESTfulAPIsAndSwagger/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Data/Converter/Contract/IParser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RestWithASPNET10Erudio.Data.Converter.Contract
-{
-    public interface IParser<O, D>
-    {
-        D Parse(O origin);
-        List<D> ParseList(List<O> origin);
-    }
-}
