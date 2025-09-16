f1196293a16c4ff6659e80f8a03f19328ade8937
leandrocgsi <leandrocgsi@gmail.com>
2025-09-16 12:06:15 -0300

    Dockerizing our ASP.NET 10 RESTful API Application

--- 30_DockerizingOurApp/RestWithASPNET10Erudio/RestWithASPNET10Erudio.Tests/IntegrationTests/Tools/SqlServerFixture.cs
+++ 30_DockerizingOurApp/RestWithASPNET10Erudio/RestWithASPNET10Erudio.Tests/IntegrationTests/Tools/SqlServerFixture.cs
@@ -1,6 +1,4 @@
-﻿using DotNet.Testcontainers.Builders;
-using Microsoft.Data.SqlClient;
-using RestWithASPNET10Erudio.Configurations;
+﻿using RestWithASPNET10Erudio.Configurations;
 using Testcontainers.MsSql;
 
 namespace RestWithASPNET10Erudio.Tests.IntegrationTests.Tools
@@ -14,35 +12,12 @@ namespace RestWithASPNET10Erudio.Tests.IntegrationTests.Tools
         public SqlServerFixture()
         {
             Container = new MsSqlBuilder()
-                .WithPassword("@Admin123")
-                .WithWaitStrategy(
-                    Wait.ForUnixContainer()
-                        .UntilMessageIsLogged(
-                        "SQL Server is now ready for client connections"))
+                .WithPassword("@Admin123$")
                 .Build();
         }
-
         public async Task InitializeAsync()
         {
             await Container.StartAsync();
-
-            // ALTERADO: garantir que a conexão realmente esteja pronta antes do Evolve
-            var retries = 10;
-            while (retries > 0)
-            {
-                try
-                {
-                    using var connection = new SqlConnection(ConnectionString);
-                    await connection.OpenAsync();
-                    break; // conexão ok
-                }
-                catch
-                {
-                    retries--;
-                    await Task.Delay(3000); // espera 3s e tenta de novo
-                }
-            }
-
             EvolveConfig.ExecuteMigrations(ConnectionString);
         }
 
